807759d4e60695dfa5ad7e090c9c2f1957582889
StefBeaulieu <stephane.beaulieu@fxinnovation.com>
2016-01-13 10:38:50 -0500

    Added script for Load Balancer script
    Script to be run for creation of Vms with internet load Balancer

---
+++ runjson.ps1
@@ -0,0 +1,17 @@
+﻿param([string]$myJson,[string]$myJsonParam,[string]$myInputFile,[string]$RGName)
+copy-item $myJsonParam "c:\script\updatedJsonParam.json"
+$myJsonParam = "c:\script\updatedJsonParam.json"
+$myArrayParam = import-csv $myInputFile
+
+foreach($myValue in $myArrayParam) {
+    (get-content $myJsonParam) |foreach-object {$_ -replace $myValue.Parameter,$myValue.Value} | set-content $myJsonParam
+}
+$RGName
+$myJson
+$myJsonParam
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName `
+                                    -TemplateFile $myJson `
+                                    -TemplateParameterFile $myJsonParam
+                                    
+                                    
